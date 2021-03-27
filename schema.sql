9b1088e3aa83d2821bb3bf2757ae8b5e385425b9
Daniel Bernal <drbernal47@gmail.com>
2021-03-27 11:21:03 -0500

    Added queries to test all loaded data and the ability to join tables on school_name

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+/* This code creates table schemas in your database */
 create table college_type(
 	school_name text Primary Key,
 	school_type text
@@ -9,3 +10,15 @@ create table salaries(
 	starting_median_salary numeric,
 	mid_career_median_salary numeric
 );
+
+
+/* Once your data has been extracted, transformed, and loaded, you can verify using these queries */
+select * from college_type;
+
+select * from salaries;
+
+/* You should be able to join data on the 'school_name' column, for example: */
+select ct.school_name, ct.school_type, s.region, s.starting_median_salary
+from college_type as ct
+join salaries as s
+on ct.school_name = s.school_name;
